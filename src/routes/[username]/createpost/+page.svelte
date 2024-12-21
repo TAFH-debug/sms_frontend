fe5336a97d77f89d1db0ec52b7ce6ede38451f9b
Xelzoid <zanggar.manarbek@icloud.com>
2024-12-21 16:15:00 +0500

    create posts

--- src/routes/[username]/createpost/+page.svelte
+++ src/routes/[username]/createpost/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts"></script>
-<div>
-    <div>
-        <textarea
-            id = "content"
-            placeholder="write content"
-            required
-        ></textarea>
-    </div>
-    <div>
-        
-    </div>
 
+<div>
+  <div>
+    <textarea id="content" placeholder="What is up my niggas?!" required></textarea>
+  </div>
+  <div>
+    <input id="images" type = "file" multiple on:change={handleFileChange}>
+    {#if images.length > 0}
+        <div>{images.length} jerked</div>
+    
+  </div>
 </div>
