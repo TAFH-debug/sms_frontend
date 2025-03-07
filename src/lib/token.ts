dad2fac5c3e5c525200a2d43adf3a33dacd8f622
TAFH-debug <talim2007@mail.ru>
2025-03-07 09:00:20 +0500

    fix: firebaseId Bad request

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -14,6 +14,7 @@ export async function register(username: string, name: string, surname: string, 
           email,
           surname,
           name,
+          firebaseId: "popa"
         }
     );
 }
